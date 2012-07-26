61d3741c7333a329d282dfd64241fcb136a42205
Bob Majdak Jr <bob@opsat.net>
2012-07-26 14:38:21 -0500

    new class to manage the frameworks auto managed instances, rather than keeping them in ::$main properties.

---
+++ lib/stash/stash.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace m {
+
+	final class stash {
+		public function __construct() { return; }
+
+		static $instances = array();
+
+		static function get($key) {
+			if(self::has($key)) return self::$instances[$key];
+			else return false;
+		}
+
+		static function has($key) {
+			if(array_key_exists($key,self::$instances)) return true;
+			else return false;	
+		}
+
+		static function set($key,$obj,$overwrite=false) {
+			// unless explicitly stated do not overwrite existing objects
+			// by default. since the idea here is a singleton manager.
+			if(!$overwrite && self::has($key))
+			throw new \Exception("already have an object named {$key}");
+
+			return self::$instances[$key] = $obj;
+		}
+
+	}
+
+}
+
+?>
