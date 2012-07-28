84f863544cd79ce12d25856a2e846f427bbeb987
Bob Majdak Jr <bob@opsat.net>
2012-07-27 21:47:43 -0400

    derp

--- lib/request/input.php
+++ lib/request/input.php
@@ -88,7 +88,7 @@ namespace m\request {
 
 		protected function filter($input) {
 			if($this->opt->trim) $input = trim($input);
-			if($this->opt->pathable) $input = request::makePathable($input,true);
+			if($this->opt->pathable) $input = request::pathable($input,true);
 			return $input;
 		}
 	}
