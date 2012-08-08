e5954bef3ebc9f5a8a19092f1e2061247899733b
Bob Majdak Jr <bob@opsat.net>
2012-08-08 12:24:20 -0700

    user library can do lots more now. still alpha.

--- lib/user/user.php
+++ lib/user/user.php
@@ -4,11 +4,10 @@ namespace m {
 	use \m as m;
 
 	class user extends m\object {
-		static $current = null;
 
 		static $PropertyMap = array(
 			'u_id'       => 'ID',
-			'u_name'     => 'Username',
+			'u_alias'    => 'Alias',
 			'u_email'    => 'Email',
 			'u_phash'    => 'PHash',
 			'u_psand'    => 'PSand',
@@ -24,17 +23,15 @@ namespace m {
 				'Database'   => null
 			));
 
-			// allow the object to build itself up from the raw database data
-			// provided via menagerie object construction.
 			parent::__construct($raw);
 
-			// support for custom or sharded databases.
+			// support separate and/or sharded databasees.
 			$this->Database = $opt->Database;
 
-			// as a preventive security measure to help prevent a developer from
-			// var_dumping something stupid, hashes are stripped from the object
-			// unless you ask for them to be kept. basically, only the login
-			// system really should be doing that.
+			// as a preventive security measure to help prevent a developer
+			// from var_dumping something stupid, hashes are stripped from
+			// the object unless you ask for them to be kept. basically, only
+			// the login system really should be doing that.
 			if(!$opt->KeepHashes) {
 				foreach(array('PHash','PSand') as $hashkey) {
 					if(property_exists($this,$hashkey))
@@ -43,6 +40,14 @@ namespace m {
 				unset($hashkey);
 			}
 
+			return;
+		}
+
+		//////////////////////////////////////////////////////////////////////
+		// session management ////////////////////////////////////////////////
+
+		public function sessionDestroy() {
+			setcookie('m_user','',-1,'/');
 			return;
 		}
 
@@ -87,7 +92,12 @@ namespace m {
 			return $user;
 		}
 
+		//////////////////////////////////////////////////////////////////////
+		// query methods /////////////////////////////////////////////////////
+
 		static function get($what,$opt=null) {
+			if(!$what) return false;
+
 			$opt = new m\object($opt,array(
 				'KeepHashes' => false,
 				'Database'   => option::get('m-user-database') or null
@@ -99,7 +109,7 @@ namespace m {
 				$where = 'u_email LIKE "%s"';
 			} else if(is_string($what)) {
 				// search by username.
-				$where = 'u_name LIKE "%s"';
+				$where = 'u_alias LIKE "%s"';
 			} else if(is_int($what)) {
 				// search by unique id.
 				$where = 'u_id=%d';
@@ -125,14 +135,270 @@ namespace m {
 				'KeepHashes' => $opt->KeepHashes
 			));
 		}
+
+		//////////////////////////////////////////////////////////////////////
+		// user creation /////////////////////////////////////////////////////
+
+		static function create($input) {
+			$input = new object($input,array(
+				'Username' => false,
+				'Password' => false,
+				'PConfirm' => false,
+				'Email'    => false
+			));
+
+			// validate the data and throw exceptions to notify of any issues
+			// that arise from the dataset.
+
+			/* Exception Codes **********************************************
+			 * 1 - missing data
+			 * 2 - invalid chars in username
+			 * 3 - password too short
+			 * 4 - invalid data types for fields
+			 * 5 - password confirm match failed
+			 * 6 - username already in use
+			 * 7 - email already in use
+			 * 8 - unknown error
+			 * 9 - username has no letters in it
+			 * 10 - username not long enough
+			 */
+
+			// check we have data and it is not empty.
+			foreach(array('Username','Password','Email') as $prop) {
+				if(!$input->{$prop}) throw new \Exception("No {$prop} specified",1);
+			}
+
+			// check that things that should be strings are strings.
+			foreach(array('Username','Password','Email') as $prop) {
+				if(!is_string($input->{$prop}))
+				throw new \Exception("Invalid datatype for {$prop}",4);
+			}
+
+			// check that the username only had valid characters.
+			if($input->Username !== request::pathable($input->Username))
+			throw new \Exception('Invalid characters in Username. (A-Z, 0-9, and Dash)',2);
+
+			// check that the username contains some letters. e.g. disallow
+			// pure numeric names so self::get can has automode.
+			if(!preg_match('/[a-z]/i',$input->Username))
+			throw new \Exception('Username must at least contain some letters.',9);
+
+			// check that the username is long enough.
+			$ulength = option::get('user-username-length');
+			if(strlen($input->Username) < $ulength)
+			throw new \Exception("Username must be at least {$ulength} characters long.",10);
+
+			// check that the password was long enough.
+			$plength = option::get('user-password-length');
+			if(strlen($input->Password) < $plength)
+			throw new \Exception("Password must be at least {$plength} characters long.",3);
+
+			// check that the requested password confirmation match matched.
+			// this is optional to allow for admin api to create users or
+			// whatever.
+			if($input->PConfirm !== false) {
+				if($input->Password !== $input->PConfirm)
+				throw new \Exception("Passwords did not match.",5);
+			}
+
+			// check that the username and email address submitted is not
+			// already in use by another account.
+			$db = new m\database;
+			$olduser = $db->queryf(
+				'SELECT u_alias,u_email FROM m_users WHERE u_alias LIKE "%s" OR u_email LIKE "%s" LIMIT 1;',
+				$input->Username,
+				$input->Email
+			)->next();
+
+			if($olduser) {
+				if(strtolower($input->Username) == $olduser->u_alias)
+					throw new \Exception("Username already in use. Choose another.",6);
+
+				if(strtolower($input->Email) == $olduser->u_email)
+					throw new \Exception("Email already associated with an account.",7);
+			}
+
+			/*
+			 * create
+			 */
+
+			$phash = hash('sha512',$input->Password);
+			$psand = hash('sha512',sprintf('%s %d',microtime(),rand(1,9001)));
+
+			$u_id = $db->queryf(
+				'INSERT INTO m_users (u_alias,u_email,u_phash,u_psand) VALUES ("%s","%s","%s","%s");',
+				$input->Username,
+				$input->Email,
+				$phash,
+				$psand
+			)->id();
+
+			if($u_id) return self::get((int)$u_id,array('KeepHashes'=>true));
+			else throw new \Exception("Unknown error creating account.",8);
+		}
+
+		//////////////////////////////////////////////////////////////////////
+		// authenticate //////////////////////////////////////////////////////
+
+		static function auth($input) {
+			$input = new m\object($input,array(
+				'Account'  => false,
+				'Password' => false
+			));
+
+			/* Exception Codes
+			 * 1 - missing data
+			 * 2 - invalid datatype
+			 * 3 - user not found
+			 * 4 - invalid password
+			 */
+
+			// check that we have our data.
+			foreach($input as $prop => $val) {
+				if(!$val) throw new \Exception("No {$prop} specified",1);
+				if(!is_string($val)) throw new \Exception("Invalid type for {$prop}",2);
+			}
+
+			// see if the account exists.
+			$user = self::get($input->Account,array('KeepHashes'=>true));
+			if(!$user) throw new \Exception('User not found',3);
+
+			// see if the passwords match.
+			if($user->PHash !== hash('sha512',$input->Password))
+			throw new \Exception('Invalid password',4);
+
+			return $user;
+		}
+
+		//////////////////////////////////////////////////////////////////////
+		// automatic post handlers ///////////////////////////////////////////
+
+		static function handlerSignup($post) {
+			try {
+				$user = self::create(array(
+					'Username' => $post->username,
+					'Password' => $post->password1,
+					'PConfirm' => $post->password2,
+					'Email'    => $post->email
+				));
+			}
+
+			catch(\Exception $e) {
+				self::handlerException($e);
+			}
+
+			//. log the new user in.
+			$user->sessionUpdate();
+
+			//. and refresh or go somewhere.
+			$bye = new m\request\redirect(($post->redirect)?($post->redirect):('m://refresh'));
+			$bye->go();
+		}
+
+		static function handlerLogin($post) {
+			try {
+				$user = self::auth(array(
+					'Account' => $post->account,
+					'Password' => $post->password
+				));
+			}
+
+			catch(\Exception $e) {
+				self::handlerException($e);
+			}
+
+			// log the user in.
+			$user->sessionUpdate();
+
+			//. and refresh or go somewhere.
+			$bye = new m\request\redirect(($post->redirect)?($post->redirect):('m://refresh'));
+			$bye->go();
+		}
+
+		static function handlerLogout($post) {
+			$user = m\stash::get('user');
+
+			if($user) {
+				$user->sessionDestroy();
+			}
+
+			$bye = new m\request\redirect('m://home');
+			$bye->go();
+		}
+
+		static function handlerException($e) {
+			$message = sprintf('Error: %s (%d)',$e->getMessage(),$e->getCode());
+
+			switch(m\platform) {
+				case 'api': {
+					$api = new m\api;
+					$api->shutdown($e->getMessage(),$e->getCode());
+					break;
+				}
+				default: {
+					$surface = m\stash::get('surface');
+					if($surface) $surface->set('error',$message);
+					else die($message);
+				}
+			}
+		}
+
+
 	}
 
 }
 
-namespace {
+namespace m {
+	m_require('-ldatabase');
 
-	m\ki::queue('m-setup',function(){
-		m\user::$current = m\user::getFromSession();
+	///////////////////////////////////////////////////////////////////////////
+	// library config /////////////////////////////////////////////////////////
+	ki::queue('m-config',function(){
+		option::define(array(
+			'user-username-length'   => 3,
+			'user-password-length'   => 6,
+			'user-enable-post-hooks' => true
+		));
+	});
+
+	///////////////////////////////////////////////////////////////////////////
+	// library setup //////////////////////////////////////////////////////////
+	ki::queue('m-setup',function(){
+		stash::set('user',($user = user::getFromSession()));
+
+		if(option::get('user-enable-post-hooks')) {
+			$post = new request\input('post');
+			if(!$post->action) return;
+
+			// if the user is not signed in register the handlers for sign up
+			// and logging in.
+			if(!$user) ki::queue('m-ready',function(){
+				$post = new request\input('post');
+				if(!$post->action) return;
+
+				switch($post->action) {
+					case 'm-signup': { user::handlerSignup($post); break; }
+					case 'm-login': { user::handlerLogin($post); break; }
+				}
+
+				return;
+			});
+
+			// if the user is logged in then register the handler for logging
+			// out.
+			else ki::queue('m-ready',function(){
+				$post = new request\input('post');
+				if(!$post->action) return;
+
+				switch($post->action) {
+					case 'm-logout': { user::handlerLogout($post); break; }
+				}
+
+				return;
+			});
+
+		}
+
 		return;
 	});
 
--- lib/user/user.sql
+++ lib/user/user.sql
@@ -3,8 +3,11 @@
 
 CREATE TABLE m_users (
 	u_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	u_name VARCHAR(16),
+	u_alias VARCHAR(16),
 	u_email VARCHAR(64),
 	u_phash VARCHAR(128),
 	u_psand VARCHAR(128)
 );
+
+CREATE INDEX u_alias ON m_users(u_alias);
+CREATE INDEX u_email ON m_users(u_email);
