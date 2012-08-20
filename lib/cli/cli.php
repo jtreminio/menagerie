0eb1affc079c60b9dc068e6550041782a5a43faa
Bob Majdak Jr <bob@opsat.net>
2012-08-20 16:01:21 -0700

    what a putz

--- lib/cli/cli.php
+++ lib/cli/cli.php
@@ -3,30 +3,30 @@
 namespace m {
 
 	class cli {
-	
+
 		public $argv = array();
 		public $args = array();
-	
+
 		public function __construct() {
 			$this->parseArguments();
-			
+
 			return;
 		}
-		
+
 		public function __get($key) {
 			if(array_key_exists($key,$this->argv)) return $this->argv[$key];
 			else return null;
 		}
-		
+
 		public function exists($key) {
 			return array_key_exists($key,$this->argv);
 		}
-		
+
 		public function shutdown() {
 			$argv = func_get_args();
 			$errno = 0;
 			$errmsg = '';
-			
+
 			// this version of the function is an experiment in
 			// pseudopolymorphism. e.g., will anybody care that it does not
 			// really care about the order of its arguments.
@@ -35,15 +35,15 @@ namespace m {
 			// - cli->shutdown(string errmsg)
 			// - cli->shutdown(int errno, string errmsg)
 			// - cli->shutdown(string errmsg, int errno)
-			
+
 			switch(count($argv)) {
 				case 1: { }
 				case 2: {
-				
+
 					foreach($argv as $arg)
 						if(is_int($arg)) $errno = $arg;
 						else if(is_string($arg)) $errmsg = $arg;
-						
+
 					break;
 				}
 				default: {
@@ -51,21 +51,21 @@ namespace m {
 					break;
 				}
 			}
-			
+
 			//. output message.
 			if($errmsg) fwrite(
 				($errno)?(STDERR):(STDOUT),
 				$errmsg.PHP_EOL
 			);
-			
+
 			//. goodbye.
 			exit($errno);
 		}
-		
+
 		protected function parseArguments() {
 			if(!array_key_exists('argv',$_SERVER)) return;
 			if(!is_array($_SERVER['argv'])) return;
-			
+
 			$input = $_SERVER['argv'];
 			unset($input[0]);
 
@@ -79,15 +79,15 @@ namespace m {
 					$this->args[] = $argv;
 				}
 			}
-			
+
 			return;
 		}
 
 		static function only() {
-			$platform = m\stash::get('platform');
+			$platform = stash::get('platform');
 			if(!$platform->cli) exit(0);
 		}
-	
+
 	}
 
 }
