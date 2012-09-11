a8ab241d1f7db86458ffa66618ce22985464f9d5
Bob Majdak Jr <bob@opsat.net>
2012-09-11 03:01:40 +0000

    updated sql to match my working table thus far

--- lib/user/user.sql
+++ lib/user/user.sql
@@ -2,11 +2,15 @@
 -- template for the user table.
 
 CREATE TABLE m_users (
-	u_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	u_alias VARCHAR(16),
-	u_email VARCHAR(64),
-	u_phash VARCHAR(128),
-	u_psand VARCHAR(128)
+	u_id            INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	u_alias         VARCHAR(16),
+	u_email         VARCHAR(64),
+	u_email_confirm VARCHAR(32),
+	u_phash         VARCHAR(128),
+	u_psand         VARCHAR(128),
+	u_jtime         INT UNSIGNED,
+	u_ltime         INT UNSIGNED,
+	u_admin         BOOL
 );
 
 CREATE INDEX u_alias ON m_users(u_alias);
