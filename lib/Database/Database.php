b9cb5d99769a17e86850540dab1e3b0b66fa8893
Bob Majdak Jr <bob@opsat.net>
2012-12-08 19:25:10 -0500

    refactor, query stats, ... fixed reuse... ...

--- lib/Database/Database.php
+++ lib/Database/Database.php
@@ -1,136 +1,183 @@
 <?php
 
-namespace m {
-	use \m as m;
+namespace m;
+use \m as m;
 
-	class Database {
+class Database {
 
-		static $dbx = array();
+	/*//
+	@property static array DBX
+	holds all the database connections opened this application instance.
+	//*/
 
-		private $driver;
+	static $DBX = array();
 
-		private function ValidateConfig($config) {
-			$require = array(
-				'driver',
-				'hostname',
-				'username','password',
-				'database'
-			);
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
 
-			foreach($require as $property) {
-				if(!property_exists($config,$property))
-				return false;
-			}
+	/*//
+	@property private object Driver
+	the database driver instance used by this object.
+	//*/
 
+	private $Driver;
+	private $Reused;
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	/*//
+	@method private boolean ValidateConfig
+	@arg object ConfigEntry
+	@flags internal
+
+	well tell you if the configuration for this database entry looks sane
+	enough to attempt to use.
+	//*/
+
+	private function ValidateConfig($config) {
+		$require = array(
+			'driver',
+			'hostname',
+			'username','password',
+			'database'
+		);
+
+		foreach($require as $property) {
+			if(!property_exists($config,$property))
+			return false;
+		}
+
+		return true;
+	}
+
+	/*//
+	@method private boolean LoadDriver
+	@arg string ConfigName
+	@arg object ConfigEntry
+	@flags internal
+
+	will attempt to load the driver requested by the configuration.
+	//*/
+
+	private function LoadDriver($name,$config) {
+		$driver = "m\\Database\\Drivers\\{$config->driver}";
+
+		//. the PSR-0 autoloader should handle the driver being loaded
+		//. when we ask for it like this.
+
+		if(!class_exists($driver,true)) {
+			return false;
+		} else {
+			$this->Driver = new $driver($name,$config);
 			return true;
 		}
-
-		private function LoadDriver($name,$config) {
-			$driver = "m\\Database\\Drivers\\{$config->driver}";
-
-			//. the PSR-0 autoloader should handle the driver being loaded
-			//. when we ask for it like this.
-
-			if(!class_exists($driver,true)) {
-				return false;
-			} else {
-				$this->driver = new $driver($name,$config);
-				return true;
-			}
-		}
+	}
 
 
-		/*// Public Database API.
-		  // The methods you will use to interact with the database.
-		  //*/
+	/*// Public Database API.
+	  // The methods you will use to interact with the database.
+	  //*/
 
-		/* database->__construct(string config);
-		 *
-		 * when a new database instance is created a new database connection
-		 * will be created using the parameters defined from the application
-		 * configuration file, and the entry in it specified.
-		 *
-		 * connections are held open by the database class in a static list
-		 * so that if another database instance is created later, the
-		 * database connection will be reused instead of recreated.
-		 *
-		 */
+	/* database->__construct(string config);
+	 *
+	 * when a new database instance is created a new database connection
+	 * will be created using the parameters defined from the application
+	 * configuration file, and the entry in it specified.
+	 *
+	 * connections are held open by the database class in a static list
+	 * so that if another database instance is created later, the
+	 * database connection will be reused instead of recreated.
+	 *
+	 */
 
-		public function __construct($which=null) {
-			if(!$which) $which = 'default';
+	public function __construct($which=null) {
+		if(!$which) $which = 'default';
 
-			//. if the requested database has already been connect to before
-			//. then we should totally reuse that connection and run with it.
-			if(array_key_exists($which,self::$dbx)) {
-				$this->driver = self::$dbx[$which];
-				return;
-			}
-
-			//. get the database configuration from the option api.
-			$config = option::get('m-database');
-			if(!$config or !is_array($config))
-				throw new \Exception('database configuration is nowhere near valid');
-
-			//. check that we have the requested config.
-			if(!array_key_exists($which,$config))
-				throw new \Exception("no valid database configuration for {$which}");
-
-			//. check that the config is good.
-			$cfg = (object)$config[$which];
-			if(!$this->validateConfig($cfg))
-				throw new \Exception("invalid configuration");
-
-			//. check that we have the required driver.
-			if(!$this->loadDriver($which,$cfg))
-				throw new \Exception("no driver for {$cfg->driver}");
-
-			//. check that we can connect.
-			if(!$this->connect($cfg)) {
-				throw new \Exception("unable to connect to database {$which}");
-			}
-
+		//. if the requested database has already been connect to before
+		//. then we should totally reuse that connection and run with it.
+		if(array_key_exists($which,self::$DBX)) {
+			$this->Driver = self::$DBX[$which];
+			$this->Reused = true;
 			return;
 		}
 
-		public function __call($func,$argv) {
-			// function connect();
-			// function disconnect();
-			// ... and anything else not specified but may be provided by drivers.
+		$this->Reused = false;
 
-			if(!method_exists($this->driver,$func))
-				throw new \Exception('requested method not found in driver.');
+		//. get the database configuration from the option api.
+		$config = Option::Get('m-database');
+		if(!$config or !is_array($config))
+			throw new \Exception('database configuration is nowhere near valid');
 
-			return call_user_func_array(array($this->driver,$func),$argv);
+		//. check that we have the requested config.
+		if(!array_key_exists($which,$config))
+			throw new \Exception("no valid database configuration for {$which}");
+
+		//. check that the config is good.
+		$cfg = (object)$config[$which];
+		if(!$this->ValidateConfig($cfg))
+			throw new \Exception("invalid configuration");
+
+		//. check that we have the required driver.
+		if(!$this->LoadDriver($which,$cfg))
+			throw new \Exception("no driver for {$cfg->Driver}");
+
+		//. check that we can connect.
+		$start = microtime(true);
+		if(!$this->Connect($cfg)) {
+			throw new \Exception("unable to connect to database {$which}");
 		}
+		$this->Driver->ConnectTime = microtime(true) - $start;
 
-		public function Queryf($fmt) {
-			$argv = func_get_args();
-			unset($argv[0]);
+		// keep for later reuse.
+		self::$DBX[$which] = $this->Driver;
 
-			/*
-			allow sprintf style use of this method, however all arguments to
-			be subsituted into the final string are escaped automatically.
-			it is intentional that the container (first argument) is not
-			escaped. there will have to be a tutorial to explain how to
-			properly use this method for optimal SQL injection protection.
-
-				`SELECT * FROM users WHERE u_email LIKE "%s";`,
-				`who@where.what`
-			*/
-
-			// protect arguments against injection.
-			foreach($argv as &$arg)
-			$arg = $this->driver->escape($arg);
-
-			// compile the finished query string.
-			$sql = vsprintf($fmt,$argv);
-
-			// do a query.
-			return $this->driver->query($sql);
-		}
-
+		return;
 	}
 
+	public function __call($func,$argv) {
+		// function connect();
+		// function disconnect();
+		// ... and anything else not specified but may be provided by drivers.
+
+		if(!method_exists($this->Driver,$func))
+			throw new \Exception('requested method not found in driver.');
+
+		return call_user_func_array(array($this->Driver,$func),$argv);
+	}
+
+	public function Queryf($fmt) {
+		$argv = func_get_args();
+		unset($argv[0]);
+
+		/*
+		allow sprintf style use of this method, however all arguments to
+		be subsituted into the final string are escaped automatically.
+		it is intentional that the container (first argument) is not
+		escaped. there will have to be a tutorial to explain how to
+		properly use this method for optimal SQL injection protection.
+
+			`SELECT * FROM users WHERE u_email LIKE "%s";`,
+			`who@where.what`
+		*/
+
+		// protect arguments against injection.
+		foreach($argv as &$arg)
+		$arg = $this->Driver->Escape($arg);
+
+		// compile the finished query string.
+		$sql = vsprintf($fmt,$argv);
+
+		// do a query.
+		$start = microtime(true);
+
+		$q = $this->Driver->Query($sql);
+
+		$this->Driver->QueryTime += (microtime(true) - $start);
+		$this->Driver->QueryCount++;
+
+		return $q;
+	}
 
 }
 
--- lib/Database/Driver.php
+++ lib/Database/Driver.php
@@ -1,35 +1,62 @@
 <?php
 
-namespace m\database {
+namespace m\database;
 
-	abstract class driver {
-	
-		protected $name;
+abstract class Driver {
 
-		protected function throwError($message) {
-			throw new \Exception("{$message} [{$this->name}]");
-		}
-	
-		public function __construct($name) {
-			$this->name = $name;
-			return;
-		}
-		
-		public function __destruct() {
-			$this->disconnect();
-			return;
-		}
-		
-		abstract public function connect($config);
-		abstract public function disconnect();
-		abstract public function escape($input);
-		abstract public function query($sql);
+	protected $Name;
 
-		// highly recommended additional methods:
-		// public function id(void); // return the last inserted id.
-	
+	public $QueryCount = 0;
+	public $QueryTime = 0;
+	public $ConnectTime = 0;
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	public function __construct($name) {
+		$this->Name = $name;
+		return;
 	}
 
+	public function __destruct() {
+		$this->Disconnect();
+		return;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	protected function ThrowError($message) {
+		throw new \Exception("{$message} [{$this->Name}]");
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	public function GetConnectTime() {
+		return sprintf('%.3fsec',$this->ConnectTime);
+	}
+
+	public function GetQueryTime() {
+		return sprintf('%.3fsec',$this->QueryTime);
+	}
+
+	public function GetQueryCount() {
+		return $this->QueryCount;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	abstract public function Connect($config);
+	abstract public function Disconnect();
+	abstract public function Escape($input);
+	abstract public function Query($sql);
+
+	// highly recommended additional methods:
+	// public function id(void); // return the last inserted id.
+
 }
+
 
 ?>
