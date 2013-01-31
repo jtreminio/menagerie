1ffeb07c3660417efa0c82bcdd21942a0f7845b4
Bob Majdak Jr <bob@opsat.net>
2013-01-31 13:27:42 -0600

    cleaning and commenting of stash

--- lib/Stash/Stash.php
+++ lib/Stash/Stash.php
@@ -1,42 +1,98 @@
 <?php
 
-namespace m {
+namespace m;
 
-	final class stash {
-		public function __construct() { return; }
+/*//
+@class m\Stash
 
-		static $instances = array();
+a static singleton class for passing objects throughout the current instance of
+the application. the stash is designed to hold objects of importance that should
+only ever exist once (e.g. singletons).
+//*/
 
-		static function get($key) {
-			if(self::has($key)) return self::$instances[$key];
-			else return false;
-		}
+final class Stash {
 
-		static function has($key) {
-			if(array_key_exists($key,self::$instances)) return true;
-			else return false;
-		}
+	/*//
+	@property static array Instances
+	an array indexing all the data stored by the stash.
+	//*/
 
-		static function set($key,$obj,$overwrite=false) {
-			// unless explicitly stated do not overwrite existing objects
-			// by default. since the idea here is a singleton manager.
-			if(!$overwrite && self::has($key))
-			throw new \Exception("already have an object named {$key}");
+	static $Instances = array();
 
-			return self::$instances[$key] = $obj;
-		}
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
 
-		static function destroy($key) {
-			if(self::has($key)) {
-				self::$instances[$key]->__destruct();
-				unset(self::$instances[$key]);
+	public function __construct() { return; }
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
+	/*//
+	@method static mixed Get
+	@arg string Key
+
+	retrieve a specific value from the stash.
+	//*/
+
+	static function Get($key) {
+		if(self::Has($key)) return self::$Instances[$key];
+		else return false;
+	}
+
+	/*//
+	@method static boolean Has
+	@arg string Key
+
+	check if there is already a key in the stash with this name.
+	//*/
+
+	static function Has($key) {
+		if(array_key_exists($key,self::$Instances)) return true;
+		else return false;
+	}
+
+	/*//
+	@method static mixed Set
+	@arg string Key
+	@arg mixed Data
+	@arg boolean Overwrite
+
+	store data under a specific key in the stash. by default the stash will
+	not overwrite anything already stored under that key and will throw an
+	exception if you try.
+	//*/
+
+	static function Set($key,$obj,$overwrite=false) {
+		// unless explicitly stated do not overwrite existing objects
+		// by default. since the idea here is a singleton manager.
+		if(!$overwrite && self::Has($key))
+		throw new \Exception("already have an object named {$key}");
+
+		return self::$Instances[$key] = $obj;
+	}
+
+	/*//
+	@method static void Destroy
+	@arg string Key
+
+	remove something from the stash. if the something is an object and the
+	destructor is accessable we will force the object to destroy itself.
+	//*/
+
+	static function Destroy($key) {
+		if(self::Has($key)) {
+			if(is_object(self::$Instances[$key])){
+				if(is_callable([self::$Instances[$key],'__destruct']))
+				self::$Instances[$key]->__destruct();
 			}
 
-			return;
+			unset(self::$Instances[$key]);
 		}
 
+		return;
 	}
 
 }
+
 
 ?>
